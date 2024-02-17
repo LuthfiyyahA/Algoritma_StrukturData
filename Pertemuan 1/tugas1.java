ae41dd9c597a22cad5eec4f2a3519111ed735d60
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-02-17 20:57:40 +0700

    Pertemuan 1

---
+++ Pertemuan 1/tugas1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char kode;
+        boolean found = false;
+        char[] KODE = { 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'L', 'N', 'T' };
+        char[][] KOTA = {
+                { 'B', 'A', 'N', 'T', 'E', 'N' },
+                { 'J', 'A', 'K', 'A', 'R', 'T', 'A' },
+                { 'B', 'A', 'N', 'D', 'U', 'N', 'G' },
+                { 'C', 'I', 'R', 'E', 'B', 'O', 'N' },
+                { 'B', 'O', 'G', 'O', 'R' },
+                { 'P', 'E', 'K', 'A', 'L', 'O', 'N', 'G', 'A', 'N' },
+                { 'S', 'E', 'M', 'A', 'R', 'A', 'N', 'G' },
+                { 'S', 'U', 'R', 'A', 'B', 'A', 'Y', 'A' },
+                { 'M', 'A', 'L', 'A', 'N', 'G' },
+                { 'T', 'E', 'G', 'A', 'L' }};
+
+        System.out.print("Masukkan Kode Plat Mobil : ");
+        kode = sc.next().toUpperCase().charAt(0);
+
+        for (int i = 0; i < KODE.length && !found; i++) {
+            if (KODE[i] == kode) {
+                System.out.println("Nama Kota : " + String.valueOf(KOTA[i]));
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Kode Plat Mobil Invalid");
+        }
+    }
+}
---
+++ Pertemuan 1/tugas2.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("____________________");
+            System.out.println("Menu:");
+            System.out.println("1. Hitung Kecepatan");
+            System.out.println("2. Hitung Jarak");
+            System.out.println("3. Hitung Waktu");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih menu: ");
+            int menu = sc.nextInt();
+            System.out.print("--------------------");
+            System.out.println("\n");
+
+            switch (menu) {
+                case 1:
+                System.out.println("Kecepatannya yaitu : " + hitungKecepatan() + " m/s");
+                System.out.print("\n");
+                    break;
+                case 2:
+                System.out.println("Jaraknya yaitu : " + hitungJarak() + " m");
+                System.out.print("\n");
+                    break;
+                case 3:
+                System.out.println("Waktunya yaitu : " + hitungWaktu() + " s");
+                System.out.print("\n");
+                    break;
+                case 4:
+                System.out.println("Terimakasih telah menggunakan program ini");
+                System.out.print("\n");
+                    return;
+                default:
+                    System.out.println("Menu tidak valid!");
+                    System.out.print("\n");
+            }
+        }
+    }
+
+    static double hitungKecepatan() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan jarak : ");
+        double s = sc.nextDouble();
+        
+        System.out.print("Masukkan waktu : ");
+        double t = sc.nextDouble();
+
+        System.out.print("\n");
+
+        return s / t;
+    }
+
+    static double hitungJarak() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan kecepatan : ");
+        double v = sc.nextDouble();
+        
+        System.out.print("Masukkan waktu     : ");
+        double t = sc.nextDouble();
+
+        System.out.print("\n");
+
+       return v * t;
+    }
+
+    static double hitungWaktu() {
+       Scanner sc = new Scanner(System.in);
+       
+       System.out.print("Masukkan jarak     : ");
+       double s = sc.nextDouble();
+       
+       System.out.print("Masukkan kecepatan : ");
+       double v = sc.nextDouble();
+
+       System.out.print("\n");
+
+      return s / v;
+   }
+}
