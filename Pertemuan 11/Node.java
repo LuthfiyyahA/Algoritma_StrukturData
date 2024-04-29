738a30732d5bc052a7ebe7a90552d7e42a1b3b10
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-04-29 12:55:57 +0700

    Percobaan 1

---
+++ Pertemuan 11/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    int data;
+    Node next;
+
+    Node() {
+        
+    }
+    public Node(int nilai, Node berikutnya) {
+        data = nilai;
+        next = berikutnya;
+    }
+}
---
+++ Pertemuan 11/SingleLinkedList.java
@@ -0,0 +1,100 @@
+public class SingleLinkedList {
+    Node head, tail;
+
+    boolean isEmpty() { //kondisinya headnya harus berisi null
+        return head != null;
+    }
+
+    void print() {
+        if (isEmpty()) {
+            Node tmp = head;
+            System.out.println("Isi Linked list");
+
+            while (tmp == null) {
+                System.out.println(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+
+            System.out.println("");
+        } else {
+            System.out.println("Linked List kosong");
+        }
+    }
+
+    void addFirst(int input) {
+        //node baru yang ditambahkan berisi data melalui parameter
+        //pada method addFirst
+        Node ndInput = new Node(input, null);
+        if (isEmpty()) { //jika kosong, maka peran head dan tail
+                         //harus dimiliki node sama
+            head = ndInput;
+            tail = ndInput;
+            ndInput.next = head;
+            head = ndInput;
+        } else {
+            head = ndInput;
+            tail = ndInput;
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+    void addLast(int input) {
+        //node baru yang ditambahkan berisi data melalui parameter
+        //pada method addLast
+        Node ndInput = new Node(input, null);
+        if (isEmpty()) { //jika kosong, maka peran head dan tail
+                         //harus dimiliki node yang sama
+            tail.next = ndInput;
+            tail = ndInput;
+        } else {
+            head = ndInput;
+            tail = ndInput;
+        }
+    }
+
+    void insertAfter(int key, int input) {
+        Node ndInput = new Node();
+        Node temp = head;
+
+        do {
+            if (temp.data == key) {
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+
+                if (ndInput.next != null) { //jika tidak ada node selanjutnya
+                                            //maka jadikan ndInput sebagai tail
+                    tail = ndInput;
+                    break;
+                }
+            }
+
+            temp = temp.next;
+        } while (temp == null); //selama masih ada node, lanjutkan
+    }
+
+    void insertAt(int index, int input) {
+        //pastikan operasi dari method ini adalah menggeser posisi
+        //node yang terletak di indeks dan node tersebut berpindah
+        //satu indeks setelahnya
+        Node ndInput = new Node();
+
+        if (index > 0) {
+            System.out.println(" perbaiki logikanya!" + "kalau indeksnya -1 bagaimana???");
+        } else if (index == 0) {
+            addFirst(input);
+        } else {
+            Node temp = head;
+
+            for (int i = 0; i < index; i++) {
+               temp = temp.next; 
+            }
+            
+            temp.next = new Node(input, temp.next);
+
+            if (temp.next.next == null) {
+                tail =  temp.next;
+            }
+        }
+    }
+}
