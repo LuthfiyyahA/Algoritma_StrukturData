8de9bce57617780b6cb40024a66bbad1e5df49c4
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-05-27 12:46:48 +0700

    Percobaan 1

---
+++ Pertemuan 14/Node15.java
@@ -0,0 +1,15 @@
+public class Node15 {
+    int data;
+    Node15 left;
+    Node15 right;
+
+    public Node15() {
+
+    }
+
+    public Node15(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
