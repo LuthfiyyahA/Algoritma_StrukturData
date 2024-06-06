acb4753216a93cc46ac50330e8296d6a069f7ae5
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-06-07 06:12:46 +0700

    Modifikasi pertanyaan 4

--- Pertemuan 16/Percobaan 1/ContohList.java
+++ Pertemuan 16/Percobaan 1/ContohList.java
@@ -25,5 +25,9 @@ public class ContohList {
         names.set(0, "My Kid");
         System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n", names.get(0), names.size(), names.get(names.size() - 1));
         System.out.println("Names: " + names.toString());
+
+        names.push("Mei-mei");
+        System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n", names.get(0), names.size(), names.get(names.size() - 1));
+        System.out.println("Names: " + names.toString());
     }
 }
